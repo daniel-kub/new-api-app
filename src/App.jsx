9586ba91d9f44e3c78b13e81844475be67428915
Daniel <daniel.kubowicz10@gmail.com>
2025-10-08 09:24:08 +0200

    sfdfd

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { Emoji } from './pages/emoji'
-import { Menu } from './pages/menu'
-import { Cytaty } from './pages/cytaty'
-import { Navbar } from './components/Navbar'
 
 function App() {
-  return <>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/emoji" element={<Emoji/>} />
-        <Route path='/menu' element={<Menu/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='*' element={<h1>404 Not Found</  h1>} />
-      </Routes>
-    </Router>
-  </>
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
